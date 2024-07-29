17c39a124fbecf4c9d3be4f8d4366822e75badc9
Feyisayo Badmus <feymus99@gmail.com>
2024-07-29 17:39:58 +0300

    Implement Product Showcase Animation

--- src/components/ProductShowcase.tsx
+++ src/components/ProductShowcase.tsx
@@ -1,7 +1,21 @@
-import appScreen from "@/assets/images/app-screen.png";
+"use client";
+
 import Image from "next/image";
+import { motion, useScroll, useTransform } from "framer-motion";
+import appScreen from "@/assets/images/app-screen.png";
+import { useRef } from "react";
 
 export const ProductShowcase = () => {
+  const imageRef = useRef<HTMLImageElement>(null);
+
+  const { scrollYProgress } = useScroll({
+    target: imageRef,
+    offset: ["start end", "end end"],
+  });
+
+  const rotateX = useTransform(scrollYProgress, [0, 1], [15, 0]);
+  const opacity = useTransform(scrollYProgress, [0, 1], [0.5, 1]);
+
   return (
     <section className="bg-gradient-to-b from-black to-[#5d2ca8] text-white text-center py-[72px] sm:py-24">
       <div className="container">
@@ -12,11 +26,20 @@ export const ProductShowcase = () => {
           Celebrate the joy of accomplishment with an app designed to track your progress,
           motivate your efforts, and celebrate your successes, one task at a time.
         </p>
-        <Image
-          src={appScreen}
-          alt="Product Screenshot"
-          className="mt-14 inline-flex mx-auto"
-        />
+        <motion.div
+          style={{
+            opacity,
+            rotateX,
+            transformPerspective: "800px",
+          }}
+        >
+          <Image
+            ref={imageRef}
+            src={appScreen}
+            alt="Product Screenshot"
+            className="mt-14 inline-flex mx-auto"
+          />
+        </motion.div>
       </div>
     </section>
   );
