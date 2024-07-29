fd701591f1512af6bd6fd92ed71bceed00c9c787
Feyisayo Badmus <feymus99@gmail.com>
2024-07-29 04:46:55 +0300

    Implement FAQ Section

--- src/components/FAQs.tsx
+++ src/components/FAQs.tsx
@@ -1,3 +1,10 @@
+"use client";
+
+import clsx from "clsx";
+import { useState } from "react";
+import PlusIcon from "@/assets/icons/plus.svg";
+import MinusIcon from "@/assets/icons/minus.svg";
+
 const items = [
   {
     question: "What payment methods do you accept?",
@@ -21,6 +28,34 @@ const items = [
   },
 ];
 
+const AccordionItem = ({ question, answer }: { question: string; answer: string }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <div className="py-5 border-b border-white/30" onClick={() => setIsOpen(!isOpen)}>
+      <div className="flex items-center">
+        <span className="flex-1 text-lg font-bold">{question}</span>
+        {isOpen ? <MinusIcon /> : <PlusIcon />}
+      </div>
+      <div className={clsx("mt-4", { hidden: !isOpen, "": isOpen === true })}>
+        {answer}
+      </div>
+    </div>
+  );
+};
+
 export const FAQs = () => {
-  return null;
+  return (
+    <section className="bg-black text-white py-[72px] sm:py-24 bg-gradient-to-b from-[#5d2ca8] to-black">
+      <div className="container max-w-2xl">
+        <h2 className="text-5xl sm:text-6xl font-bold text-center tracking-tighter">
+          Frequently asked questions
+        </h2>
+        <div className="mt-12">
+          {items.map(({ question, answer }, index) => (
+            <AccordionItem answer={answer} question={question} key={question} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 };
